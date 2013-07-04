5fd8fe5cdaff339026e17f488b718e545d4126f6
Laurent <lchenay@lafourchette.com>
2013-07-04 16:46:09 +0200

    Register all commands

--- src/console.php
+++ src/console.php
@@ -10,5 +10,9 @@ $console = new Application('My Silex Application', 'n/a');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 
 \LaFourchette\Console\Status::register($app, $console);
+\LaFourchette\Console\Check::register($app, $console);
+\LaFourchette\Console\Create::register($app, $console);
+\LaFourchette\Console\Delete::register($app, $console);
+\LaFourchette\Console\Reset::register($app, $console);
 
 return $console;
