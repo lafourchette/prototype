749c648d7447d2f7cef54549b2d33d8d4189cea9
Laurent <lchenay@lafourchette.com>
2013-07-04 16:25:58 +0200

    Add other command

---
+++ src/LaFourchette/Console/Check.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace LaFourchette\Console;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Check extends ConsoleAbstract
+{
+    /**
+     * @param \Silex\Application $app
+     * @param Application $console
+     */
+    static public function register(\Silex\Application $app, Application $console)
+    {
+        $console->register('prototype:check')
+            ->setDefinition(array(
+                // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
+            ))
+            ->setDescription('Check a VM')
+            ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+                $command = new Status();
+                $command->setApplication($app);
+                $command->run($input, $output);
+            });
+    }
+
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('check');
+    }
+}
---
+++ src/LaFourchette/Console/Reset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace LaFourchette\Console;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Reset extends ConsoleAbstract
+{
+    /**
+     * @param \Silex\Application $app
+     * @param Application $console
+     */
+    static public function register(\Silex\Application $app, Application $console)
+    {
+        $console->register('prototype:reset')
+            ->setDefinition(array(
+                // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
+            ))
+            ->setDescription('Reset a VM')
+            ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+                $command = new Status();
+                $command->setApplication($app);
+                $command->run($input, $output);
+            });
+    }
+
+    public function run(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('reset');
+    }
+}
