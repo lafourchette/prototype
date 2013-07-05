4492c72e922c7e7884dc06bc6e2d4e144dff144b
Laurent <lchenay@lafourchette.com>
2013-07-05 10:02:21 +0200

    Fix interface

--- src/LaFourchette/Provisioner/ProvisionerInterface.php
+++ src/LaFourchette/Provisioner/ProvisionerInterface.php
@@ -1,13 +1,15 @@
 <?php
 namespace LaFourchette\Provisioner;
 
+use LaFourchette\Model\VM;
+
 interface ProvisionerInterface
 {
-    public function getStatus($vm);
-    public function start($vm);
-    public function stop($vm);
+    public function getStatus(VM $vm);
+    public function start(VM $vm);
+    public function stop(VM $vm);
 
-    public function initialise($vm);
+    public function initialise(VM $vm);
 
-    public function reset($vm);
+    public function reset(VM $vm);
 }
--- src/LaFourchette/Provisioner/Vagrant.php
+++ src/LaFourchette/Provisioner/Vagrant.php
@@ -1,7 +1,66 @@
 <?php
 namespace LaFourchette\Provisioner;
 
+use LaFourchette\Model\VM;
+use Symfony\Component\Process\Process;
+
 class Vagrant extends ProvisionerAbstract
 {
+    protected $depot = 'git@github.com:lafourchette/lafourchette-vm.git';
 
+
+    protected function getPrefixCommand($integ)
+    {
+        $cmd = '';
+
+
+        $sshUser = $integ->getSshUser();
+        $sshKey = $integ->getSshKey();
+        $server = $integ->getServer();
+
+        if (trim($sshUser) != '' && trim($server) != '') {
+            $cmd .= 'ssh ' . $sshUser . '@' . $server . ':';
+        }
+
+        $path = $integ->getPath();
+
+        if (trim($path) == '') {
+            $cmd .= $path;
+        } else {
+            throw new \Exception('Seriously ? no path ? I can deploy the VM everywhere ?');
+        }
+
+        return $cmd;
+    }
+
+    public function getStatus(VM $vm)
+    {
+        // TODO: Implement getStatus() method.
+    }
+
+    public function start(VM $vm)
+    {
+        $integ = $vm->getInteg();
+
+        $cmd = 'git clone git@github.com:lafourchette/lafourchette-vm.git';
+        $cmd = $this->getPrefixCommand($cmd);
+
+
+
+    }
+
+    public function stop(VM $vm)
+    {
+        // TODO: Implement stop() method.
+    }
+
+    public function initialise(VM $vm)
+    {
+        // TODO: Implement initialise() method.
+    }
+
+    public function reset(VM $vm)
+    {
+        // TODO: Implement reset() method.
+    }
 }
