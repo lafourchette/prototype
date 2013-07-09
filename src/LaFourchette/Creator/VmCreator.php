566653f70cca05eabe0cf1eb60affd9caae90823
Guillaume Cavana <gcavana@lafourchette.com>
2013-07-09 18:39:00 +0200

    Add name generator

--- src/LaFourchette/Creator/VmCreator.php
+++ src/LaFourchette/Creator/VmCreator.php
@@ -12,6 +12,7 @@ use LaFourchette\Entity\Vm;
  */
 class VmCreator implements CreatorInterface
 {
+
     protected $integDecider;
 
     public function __construct(IntegDecider $integDecider)
@@ -27,19 +28,30 @@ class VmCreator implements CreatorInterface
         $user->setIdUser(1);
         $user->setUsername('guillaume_cavana');
         $user->setEmail('gcavana@lafourchette.com');
-        
+
         $expiredAt = new \DateTime();
         $expiredAt->add(new \DateInterval(sprintf('PT%dH', Vm::EXPIRED_AT_DEFAULT_VALUE)));
 
         $vm = new Vm();
-        $vm->setName($name);
+        $vm->setName('VM-'.$this->generateRandomString());
         $vm->setCreatedBy($user); //TODO add user through ldap
         $vm->setCreateDt(new \DateTime());
         $vm->setUpdateDt(new \DateTime());
         $vm->setExpiredDt($expiredAt);
         $vm->setInteg($integ);
-        
+
         return $vm;
+    }
+
+    //@TODO add it in a tool class
+    private function generateRandomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
     }
 
     public function getName()
