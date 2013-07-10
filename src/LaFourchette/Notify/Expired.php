e65a3b876249eac03b3961896b1e67f906118dd6
Laurent <lchenay@lafourchette.com>
2013-07-10 16:47:44 +0200

    Change text for expired message

--- src/LaFourchette/Notify/Expired.php
+++ src/LaFourchette/Notify/Expired.php
@@ -6,6 +6,21 @@ use \LaFourchette\Entity\Vm;
 
 class Expired extends NotifyAbstract
 {
+    /**
+     * @param Vm $vm
+     * @return string
+     */
+    public function getSubject(Vm $vm) {
+        $integ = $vm->getInteg();
+        $name = $integ->getName();
+
+        return sprintf('Votre environnement de test %s a expiré', $name);
+    }
+
+    /**
+     * @param Vm $vm
+     * @return string
+     */
     public function getContent(Vm $vm)
     {
         $integ = $vm->getInteg();
@@ -17,7 +32,7 @@ class Expired extends NotifyAbstract
         $str = <<<EOS
 Bonjour,
 
-La VM {$name} a expiré et va être détruire d'ici quelques instants.
+L'environnement de test {$name} a expiré et va être détruire d'ici quelques instants.
 
 En espérant que la recette du/des projets a été fructueuse.
 
