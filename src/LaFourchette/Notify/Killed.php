46068e209951514474bdd5fc1e9d77f3297f771d
Laurent <lchenay@lafourchette.com>
2013-07-10 16:49:05 +0200

    Change text for killed message

--- src/LaFourchette/Notify/Killed.php
+++ src/LaFourchette/Notify/Killed.php
@@ -6,6 +6,10 @@ use \LaFourchette\Entity\Vm;
 
 class Killed extends NotifyAbstract
 {
+    /**
+     * @param Vm $vm
+     * @return string
+     */
     public function getContent(Vm $vm)
     {
         $integ = $vm->getInteg();
@@ -17,7 +21,7 @@ class Killed extends NotifyAbstract
         $str = <<<EOS
 Bonjour,
 
-La VM {$name} a cessé de fonctionner inopinément.
+L'environnement {$name} a cessé de fonctionner inopinément.
 Il s'agit d'un cas exceptionnel qui ne devrait pas se reproduire.
 
 Si cela resurvient, merci de contacter l'administrateur
@@ -26,5 +30,17 @@ Cordialement
 EOS;
 
         return $str;
+    }
+
+    /**
+     * @param Vm $vm
+     * @return string
+     */
+    public function getSubject(Vm $vm)
+    {
+        $integ = $vm->getInteg();
+        $name = $integ->getName();
+
+        return sprintf('Une erreur est survenue sur l\'environnement %s.', $name);
     }
 }
