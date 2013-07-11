1d68d8cd046f5e0b5de6dc28f2bd240b6e53c92c
Laurent <lchenay@lafourchette.com>
2013-07-11 14:47:08 +0200

    Add archived and started status in extension

--- src/LaFourchette/Twig/Extensions/LaFourchettePrototypeExtension.php
+++ src/LaFourchette/Twig/Extensions/LaFourchettePrototypeExtension.php
@@ -52,7 +52,12 @@ class LaFourchettePrototypeExtension extends \Twig_Extension
             case Vm::TO_START:
                 return '<i class="label label-info">To start</i>';
                 break;
-            
+            case Vm::STARTED:
+                return '<i class="label label-info">Currently started</i>';
+                break;
+            case Vm::ARCHIVED:
+                return '<i class="label label-info">Archived</i>';
+                break;
             default:
                 return 'no status available';
                 break;
