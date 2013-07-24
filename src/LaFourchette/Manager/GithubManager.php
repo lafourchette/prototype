cd6c3a10a81feb5aa87c1a200148a8b0878720ef
Laurent <lchenay@lafourchette.com>
2013-07-24 16:44:47 +0200

    Use in prio feature-puppetized

--- src/LaFourchette/Manager/GithubManager.php
+++ src/LaFourchette/Manager/GithubManager.php
@@ -34,12 +34,32 @@ class GithubManager
             $branches = $this->client->api('repo')->branches('lafourchette', $project->getName());
             $repositories[$key]['name'] = $project->getName();
             $repositories[$key]['id'] = $project->getIdProject();
+
             if (!empty($branches)) {
+                $selected = false;
                 foreach ($branches as $branch) {
-                    $repositories[$key]['branches'][$branch['name']] = $branch['name'] !== 'master' ? null : 'selected';
+                    $repositories[$key]['branches'][$branch['name']] = null;
+
+                    switch ($branch['name']) {
+                        case 'master':
+                                if (!$selected) {
+                                    $repositories[$key]['branches']['master'] = 'selected';
+                                    $selected = true;
+                                }
+                            break;
+                        case 'feature-puppetized':
+                            $repositories[$key]['branches']['feature-puppetized'] = 'selected';
+
+                            if ($selected) {
+                                $repositories[$key]['branches']['master'] = null;
+                            }
+                            $selected = true;
+                            break;
+                    }
                 }
             }
         }
+
         return $repositories;
     }
 
