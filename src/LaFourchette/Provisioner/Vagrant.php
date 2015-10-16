66324be0667e591c42f1c700bcd4c9344bc412d2
govi jeremy <jgovi@lafourchette.com>
2015-10-16 16:42:48 +0200

    Update Vagrant.php

--- src/LaFourchette/Provisioner/Vagrant.php
+++ src/LaFourchette/Provisioner/Vagrant.php
@@ -304,7 +304,7 @@ Vagrant.configure("2") do |config|
     config.vm.provider :virtualbox do |vb|
         vb.customize ["modifyvm", :id, "--natdnshostresolver1", "off"]
         vb.customize ["modifyvm", :id, "--natdnsproxy1", "off"]
-        vb.customize ["modifyvm", :id, "--memory", "8064"]
+        vb.customize ["modifyvm", :id, "--memory", "32768"]
         vb.customize ["modifyvm", :id, "--cpus", "8"]
     end
     config.vm.provision "shell", path: "install.sh"
